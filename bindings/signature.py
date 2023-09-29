7e496e577e2655c5ae3ec114c94390c23dad06eb
dahmed <dahmed@ncsu.edu>
2023-09-29 21:08:55 +0000

    updated signature module to use string messags

--- bindings/signature.py
+++ bindings/signature.py
@@ -1,6 +1,7 @@
 from . import ec
 from collections import namedtuple
 from .schemes import (PrivateKey, BasicSchemeMPL)
+from . import util as utils
 
 Keys = namedtuple('Keys', ['sk', 'pk'])
 
@@ -10,12 +11,15 @@ def key_gen(seed: bytes) -> Keys:
     
     return Keys(sk, pk)
 
-def sign(sk: PrivateKey, message: bytes) -> ec.G2Element:
+def sign(sk: PrivateKey, message: str) -> ec.G2Element:
+    message = utils.to_bytes(message)
+
     signature: ec.G2Element = BasicSchemeMPL.sign(sk, message)
     
     return signature
 
-def verify(pk: ec.G1Element, message: bytes, signature: ec.G2Element) -> bool:
+def verify(pk: ec.G1Element, message: str, signature: ec.G2Element) -> bool:
+    message = utils.to_bytes(message)
     ok: bool = BasicSchemeMPL.verify(pk, message, signature)
     
     return ok
